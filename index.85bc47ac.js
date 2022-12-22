78fe267fea082a3cf38cb6f5452e95e42e8e0c9e
BolotinAlexey <62134373+BolotinAlexey@users.noreply.github.com>
2022-12-22 20:32:05 +0000

    Deploying to gh-pages from @ BolotinAlexey/go-it-team-project@886160fb628d705208277a335c70360d79226a60 🚀

---
+++ index.85bc47ac.js
@@ -0,0 +1,2 @@
+!function(){for(var e=function(e){t[e].addEventListener("click",(function(){return n.style.transform="translateX(".concat(100*-e/3,"%)")}))},t=document.querySelectorAll(".gallery__button"),n=document.querySelector(".gallery__slides"),r=0;r<t.length;r++)e(r)}();
+//# sourceMappingURL=index.85bc47ac.js.map
---
+++ index.e18dcebb.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".gallery__button"),t=document.querySelector(".gallery__slides");for(let l=0;l<e.length;l++)e[l].addEventListener("click",(()=>t.style.transform=`translateX(${100*-l/3}%)`));
+//# sourceMappingURL=index.e18dcebb.js.map
