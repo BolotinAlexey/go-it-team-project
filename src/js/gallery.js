91b39feb7e0d3005236609d4e9b46c1f9a8cf3b4
Oleksii Bolotin <samkiral73@gmail.com>
2022-12-22 19:55:02 +0100

    finished slider

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,17 +1,17 @@
 const buttons = document.querySelectorAll('.gallery__button');
-const slides = document.querySelectorAll('.gallery__item');
-console.log(slides);
-slides[0].classList.add('slide-visible');
+const slides = document.querySelector('.gallery__slides');
+
+// slides[0].classList.add('slide-visible');
+
 for (let i = 0; i < buttons.length; i++) {
-  console.log(slides[i]);
   buttons[i].addEventListener(
     'click',
-    () => {
-      let oldVisible = document.querySelector('.slide-visible');
-      oldVisible.classList.remove('slide-visible');
-      slides[i].classList.add('slide-visible');
-    }
-
-    // () => (slides.style.transform = `translateX(${(-i * 100) / 3}%)`)
+    () => (slides.style.transform = `translateX(${(-i * 100) / 3}%)`)
   );
 }
+
+// () => {
+//   let oldVisible = document.querySelector('.slide-visible');
+//   oldVisible.classList.remove('slide-visible');
+//   slides[i].classList.add('slide-visible');
+// };
