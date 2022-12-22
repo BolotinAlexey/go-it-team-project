527916a9bd28fcc943f61513b2d96487667f2c0a
Oleksii Bolotin <samkiral73@gmail.com>
2022-12-22 09:15:06 +0100

    change js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,11 +1,17 @@
 const buttons = document.querySelectorAll('.gallery__button');
-const slides = document.querySelector('.gallery__slides');
-console.log(buttons);
-
+const slides = document.querySelectorAll('.gallery__item');
+console.log(slides);
+slides[0].classList.add('slide-visible');
 for (let i = 0; i < buttons.length; i++) {
   console.log(slides[i]);
   buttons[i].addEventListener(
     'click',
-    () => (slides.style.transform = `translateX(${(-i * 100) / 3}%)`)
+    () => {
+      let oldVisible = document.querySelector('.slide-visible');
+      oldVisible.classList.remove('slide-visible');
+      slides[i].classList.add('slide-visible');
+    }
+
+    // () => (slides.style.transform = `translateX(${(-i * 100) / 3}%)`)
   );
 }
