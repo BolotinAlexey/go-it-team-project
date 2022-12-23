63f9305e4432fd7fe9959f767c9436877c140d0a
ZhuravskyOleh <113792039+ZhuravskyOleh@users.noreply.github.com>
2022-12-23 13:20:48 +0000

    Deploying to gh-pages from @ BolotinAlexey/go-it-team-project@ca9733ad5881519cf5786b4a5fa305c2bfcbe032 🚀

--- index.85bc47ac.js
+++
@@ -1,2 +0,0 @@
-!function(){for(var e=function(e){t[e].addEventListener("click",(function(){return n.style.transform="translateX(".concat(100*-e/3,"%)")}))},t=document.querySelectorAll(".gallery__button"),n=document.querySelector(".gallery__slides"),r=0;r<t.length;r++)e(r)}();
-//# sourceMappingURL=index.85bc47ac.js.map
